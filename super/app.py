dd283c85bb3c54273a5df4280a3a8ef51183f8fd
Kizyma Oleg <oleg1203@gmail.com>
2024-12-05 15:30:02 +0000

    Auto-sync from server

--- super/app.py
+++ super/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template, redirect, url_for, render_template_string
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import os
