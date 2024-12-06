966243e1df9c51e47c4c751cd43dad8f22bd9f6c
Oleg Kizyma <oleg1203@gmail.com>
2024-12-06 14:57:42 +0000

    Рефакторинг модуля аутентификации: перемещение маршрутов и шаблонов в новый пакет auth_bp

--- app.py
+++ app.py
@@ -4,7 +4,18 @@
 #-----------------------------------------------------------------------------------------
 
 from flask import Flask
+from auth_bp import auth_bp
+from config import Config
+from models import db
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# Initialize extensions
+db.init_app(app)
+
+# Register blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route("/")
 def hello():
--- super/auth_bp/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth_bp = Blueprint('auth_bp', __name__, url_prefix='/auth',
-                   template_folder='../templates/auth_bp')
-
-from . import routes
